354c979446b84f66d1cc327b3342b5cee6a1d313
JlSanchez05 <luisdevops0@gmail.com>
2022-12-13 21:51:05 -0500

    added images, css styling, anchor tags, grid display

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 // import Links from './links' 
 import './style.css' 
 import DevImg from './images/dev.jpeg'
-import Dev from './dev'
+import linkIcon from './images/linkIcon.png'
+import Dev from './dev' 
+import Link from './link'
 
 function App(){
     return(
-        <header>
+        <div>
+         <header>
             <div className='info'>
                 <div className='info-img'><Dev image={DevImg}></Dev></div>
                 <div className='info-name'><h1>Dev</h1></div>
-
             </div>
         </header>
+
+        <section id='links'>
+            <div className='links-main-con'>
+                <div className='links-con'>
+                    <div className='link one'><Link icon={linkIcon} info='My Portfolio'></Link></div>
+                    <div className='link two'><Link icon={linkIcon} info='Busoness Blog'></Link ></div>
+                    <div className='link three'><Link icon={linkIcon} info='Forbes Interview'></Link ></div>
+                    <div className='link four'><Link icon={linkIcon} info='Stocks'></Link ></div>
+
+                </div>
+            </div>
+           
+        </section>
+
+
+
+
+
+
+        </div>
+  
+
+       
     )
 } 
 
